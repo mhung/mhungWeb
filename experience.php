c037818d1ba4b35548bb037a2fbaf1d14abb6fdd
Maritza <maritzahungk@gmail.com>
2015-05-30 13:48:44 -0400

    fixed size for intern pages
    fixed h1 size (title) for intern pages

--- experience.php
+++ experience.php
@@ -19,9 +19,9 @@
     <div class="container background-banner background-inner">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
            
-                    <div class="container banner-title-center">
-                        <h1 id="title-me">EXPERIENCE</h1>
-                    </div>
+                <div class="container banner-title-center">
+                    <h1 id="title-me">EXPERIENCE</h1>
+                </div>
                
            
                 
