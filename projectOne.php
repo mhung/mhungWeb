9bda3837c48359b413c68f6411040914d302ec9a
Maritza <maritzahungk@gmail.com>
2015-05-31 13:48:57 -0400

    Edited page
    edited size for columns and added carousel

--- projectOne.php
+++ projectOne.php
@@ -5,9 +5,34 @@
         <h3> COCINA ECONOMICA</h3>
     
     </div>
-    <div class="col-md-12">
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
     
-        <img src="images/cocina-economica.png" class="img-responsive"/>
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="images/scraper-project/scraper-home.jpg" alt="...">
+   
+    </div>
+    <div class="item">
+      <img src="images/scraper-project/scraper-reviews.jpg" alt="...">
+     
+    </div>
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+        
     
     </div>
     
