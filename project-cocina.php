400bf2343934817493b82b4b2377eb41682c83d0
Maritza <maritzahungk@gmail.com>
2015-05-31 13:48:34 -0400

    Added new file for cocina-economica
    Added new file for cocina-economica

---
+++ project-cocina.php
@@ -0,0 +1,46 @@
+
+<div class="container">
+    
+    <div class="col-md-12">
+        <h3> COCINA ECONOMICA</h3>
+    
+    </div>
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+    
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="images/cocina-economica-project/menu-cocina-economica.jpg" alt="...">
+   
+    </div>
+    <div class="item">
+      <img src="images/cocina-economica-project/photo-cocina.jpg" alt="...">
+     
+    </div>
+      
+    <div class="item">
+      <img src="images/cocina-economica-project/delivery-cocina-economica.jpg" alt="...">
+     
+    </div>
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+        
+    
+    </div>
+    
+
+
+</div>
